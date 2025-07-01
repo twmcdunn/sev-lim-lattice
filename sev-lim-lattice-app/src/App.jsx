13886331761be84d1455de8f63f1118f9618a0d1
Tim McDunn <twmcdunn@gmail.com>
2025-07-01 08:18:57 -0500

    updates

--- sev-lim-lattice-app/src/App.jsx
+++ sev-lim-lattice-app/src/App.jsx
@@ -1,27 +1,50 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Canvas} from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, PerspectiveCamera} from '@react-three/drei'
 
 //docs: https://r3f.docs.pmnd.rs/api/canvas
 //docs: https://threejs.org/manual/#en/fundamentals
+
+function Scene() {
+  const camRef = useRef();
+  const meshRef = useRef();
+
+  useFrame(() => {
+    if (camRef.current && meshRef.current) {
+      camRef.current.lookAt(meshRef.current.position);
+    }
+  }
+  );
+  return (
+    <>
+      <PerspectiveCamera
+        makeDefault
+        position={[5, 5, 5]}
+        ref={camRef}
+      />
+      <OrbitControls />
+      <mesh ref={meshRef}>
+        <boxGeometry args={[2, 2, 2]} />
+        <meshStandardMaterial />
+      </mesh>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[5, 5, 5]} color="red" />
+    </>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div id="canvas-container">
-    <Canvas>
-      <mesh>
-        <boxGeometry args = {[2, 2, 2]} />
-        <meshStandardMaterial />
-      </mesh>
-      <ambientLight intensity={0.1} />
-      <directionalLight position={[0,0,5]} color="red" />
-
-    </Canvas>
-   </div>
+    <div id="canvas-container">
+      <Canvas>
+        <Scene />
+      </Canvas>
+    </div>
   );
 }
 
